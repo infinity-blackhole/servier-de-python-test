3363eddb0b6a7240a0dba3d246a058ffb72afcaf
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-12-17 23:59:51 +0100

    Implement pipeline

---
+++ src/servier_de_python/pipeline.py
@@ -0,0 +1,91 @@
+"""Pipeline to find drug mentions in clinical trials and pubmed articles. """
+
+import argparse
+import typing
+
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+from servier_de_python.io import (
+    ReadFromClinicalTrials,
+    ReadFromDrugs,
+    ReadFromPubmed,
+    WriteDrugMention,
+    Drug,
+)
+from servier_de_python.transforms import (
+    SplitClinicalTrialTitleByWord,
+    SplitPubmedTitleByWord,
+    MatchClinicalTrialDrugs,
+    MatchPubmedDrugs,
+)
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--clinical-trials-dataset-path",
+        required=True,
+        help="Path to the clinical trials dataset.",
+        nargs="+",
+    )
+    parser.add_argument(
+        "--drugs-dataset-path",
+        required=True,
+        help="Path to the drugs dataset.",
+        nargs="+",
+    )
+    parser.add_argument(
+        "--pubmed-dataset-path",
+        required=True,
+        help="Path to the pubmed dataset.",
+        nargs="+",
+    )
+    parser.add_argument(
+        "--output", required=True, help="Output file to write results to."
+    )
+    args, options = parser.parse_known_args()
+
+    with beam.Pipeline(options=PipelineOptions(options)) as p:
+        clinical_trials = (
+            tuple(
+                p | f"ReadFromClinicalTrials{i}" >> ReadFromClinicalTrials(path)
+                for i, path in enumerate(args.clinical_trials_dataset_path)
+            )
+            | "FlattenClinicalTrial" >> beam.Flatten()
+            | SplitClinicalTrialTitleByWord()
+        )
+        pubmed = (
+            tuple(
+                p | f"ReadFromPubmed{i}" >> ReadFromPubmed(path)
+                for i, path in enumerate(args.pubmed_dataset_path)
+            )
+            | "FlattenPubmed" >> beam.Flatten()
+            | SplitPubmedTitleByWord()
+        )
+        drugs = (
+            tuple(
+                p | f"ReadFromDrugs{i}" >> ReadFromDrugs(path)
+                for i, path in enumerate(args.drugs_dataset_path)
+            )
+            | "FlattenDrug" >> beam.Flatten()
+            | beam.Map(lambda element: (element.name, element)).with_output_types(
+                typing.Tuple[str, Drug]
+            )
+        )
+
+        clinical_trials_mentions = (drugs, clinical_trials) | MatchClinicalTrialDrugs()
+        pubmed_mentions = (drugs, pubmed) | MatchPubmedDrugs()
+
+        (
+            (
+                clinical_trials_mentions,
+                pubmed_mentions,
+            )
+            | "FlattenDrugMention" >> beam.Flatten()
+            | WriteDrugMention(args.output)
+        )
+
+
+if __name__ == "__main__":
+    run()
